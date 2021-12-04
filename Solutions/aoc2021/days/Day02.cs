ab82583381d6a020c1c9d6243a7e661716b67e31
Cam Thompson <camstercammy@hotmail.com>
2021-12-04 00:36:51 -0500

    Adding some solutions

---
+++ Solutions/aoc2021/days/Day02.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventLibrary;
+
+namespace aoc2021
+{
+    public class Day02: ISolver
+    {
+        public Solution Solve(string filePath)
+        {
+            /*
+            var letters = AdventLibrary.StringParsing.GetStringBetweenTwoCharacters(input, "a", "b");
+            var letters = AdventLibrary.StringParsing.GetLettersFromString(input);
+            var letters = AdventLibrary.StringParsing.GetLettersFromString(input);
+            var digits = AdventLibrary.StringParsing.GetDigitsFromString(input);
+            var sub = item.Substring(0, 1);
+            Console.WriteLine("Part 1: " + Part1.ToString());
+            */
+            return new Solution(Part1(filePath), Part2(filePath));
+        }
+
+        private object Part1(string filePath)
+        {
+            var strings = AdventLibrary.ParseInput.GetLinesFromFile(filePath);
+            var hor = 0;
+            var ver = 0;
+
+            foreach (var item in strings)
+            {
+                var letters = AdventLibrary.StringParsing.GetLettersFromString(item);
+                var digits = AdventLibrary.StringParsing.GetDigitsFromString(item);
+                if (letters.StartsWith("f"))
+                {
+                    hor+= digits.First();
+                }
+                else if (letters.StartsWith("d"))
+                {
+                    ver+= digits.First();
+                }
+                else if (letters.StartsWith("u"))
+                {
+                    ver-= digits.First();
+                }
+            }
+            return ver*hor;
+        }
+        
+        private object Part2(string filePath)
+        {
+            var strings = AdventLibrary.ParseInput.GetLinesFromFile(filePath);
+            var hor = 0;
+            var dep = 0;
+            var aim = 0;
+
+            foreach (var item in strings)
+            {
+                var letters = AdventLibrary.StringParsing.GetLettersFromString(item);
+                var digits = AdventLibrary.StringParsing.GetDigitsFromString(item);
+                if (letters.StartsWith("f"))
+                {
+                    hor+= digits.First();
+                    dep += (digits.First()*aim);
+                }
+                else if (letters.StartsWith("d"))
+                {
+                    aim += digits.First();
+                }
+                else if (letters.StartsWith("u"))
+                {
+                    aim -= digits.First();
+                }
+            }
+            return dep*hor;
+        }
+    }
+}

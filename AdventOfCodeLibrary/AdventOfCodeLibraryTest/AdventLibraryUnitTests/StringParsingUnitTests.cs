6d5ba32d89d97a8a428c335b9b7b60b67771a00c
Cam Thompson <camstercammy@hotmail.com>
2021-12-13 00:41:14 -0500

    some unit tests (need more, forever, always)

---
+++ AdventOfCodeLibrary/AdventOfCodeLibraryTest/AdventLibraryUnitTests/StringParsingUnitTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+
+namespace AdventLibraryUnitTests
+{
+    public class StringParsingUnitTests
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void GetNumbersFromString_ReturnsExpected(List<int> expected, string input)
+        {
+            var result = AdventLibrary.StringParsing.GetNumbersFromString(input);
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { new List<int>() { 1, 2, 3, 4}, "1_2+3 4" },
+            new object[] { new List<int>() { 10, 20, 30, 40}, "10_20+30 40" },
+            new object[] { new List<int>() { 432, 708, 432, 160}, "432,708 -> 432,160" },
+            new object[] { new List<int>() { 579, 594, 579, 448}, "579,594 -> 579,448" },
+            new object[] { new List<int>() { 10, 10, 20, 20}, "10:10, 20:20" },
+        };
+    }
+}

96901a0aa0b09158000c5300c23c2ff44671dce0
Cameron Thompson <cameron.thompson@magnetforensics.com>
2023-06-26 13:26:09 -0400

    bad start to day 20

---
+++ Solutions/aoc2015/days/Day20.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventLibrary;
+using AdventLibrary.Helpers;
+
+namespace aoc2015
+{
+    public class Day20: ISolver
+    {
+        private string _filePath;
+        private char[] delimiterChars = { ' ', ',', '.', ':', '-', '>', '<', '+', '\t' };
+        public Solution Solve(string filePath)
+        {
+            _filePath = filePath;
+            return new Solution(Part1(), Part2());
+        }
+
+        private object Part1()
+        {
+			var numbers = ParseInput.GetNumbersFromFile(_filePath);
+            var input = numbers[0];
+
+            for (var i = 1000000; i < input/10; i++)
+            {
+                var total = 0;
+                for (var j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        total += j * 10;
+                    }
+                }
+
+                if (total >= input)
+                {
+                    return total;
+                }
+            }
+            return 0;
+        }
+
+        private object Part2()
+        {
+            return 0;
+        }
+    }
+}

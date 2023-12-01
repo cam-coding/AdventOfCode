1e756c8542e96afd1af4a48a453b45cc861a0b49
Cam Thompson <camstercammy@hotmail.com>
2023-12-01 14:37:44 -0500

    day1 and some cleanup

--- Solutions/aoc2023/days/Day01.cs
+++ Solutions/aoc2023/days/Day01.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using AdventLibrary;
 using AdventLibrary.Helpers;
@@ -19,35 +20,71 @@ namespace aoc2023
         private object Part1()
         {
             var lines = ParseInput.GetLinesFromFile(_filePath);
-			var numbers = ParseInput.GetNumbersFromFile(_filePath);
-            var nodes = ParseInput.ParseFileAsGraph(_filePath);
-            var grid = ParseInput.ParseFileAsGrid(_filePath);
-            var total = 1000000;
-			var counter = 0;
-			
+            var counter = 0;
 			foreach (var line in lines)
 			{
-                var tokens = line.Split(delimiterChars);
-				var nums = AdventLibrary.StringParsing.GetNumbersFromString(line);
-                
-				foreach (var num in nums)
-				{
-				}
-
-                for (var i = 0; i < 0; i++)
-                {
-                    for (var j = 0; j < 0; j++)
-                    {
-                        
-                    }
-                }
+				// var nums = AdventLibrary.StringParsing.GetNumbersFromString(line);
+                var nums = AdventLibrary.StringParsing.GetDigitsFromString(line);
+                var sum = nums[0] + nums[nums.Count-1];
+                var blah = string.Empty + nums[0] + nums[nums.Count - 1];
+                counter += int.Parse(blah);
 			}
-            return 0;
+            return counter;
         }
-        
+
         private object Part2()
         {
-            return 0;
+            var dict = new Dictionary<string, int>()
+            {
+                { "one", 1 },
+                { "two", 2 },
+                { "three", 3 },
+                { "four", 4 },
+                { "five", 5 },
+                { "six", 6 },
+                { "seven", 7 },
+                { "eight", 8 },
+                { "nine", 9 },
+                { "1", 1 },
+                { "2", 2 },
+                { "3", 3 },
+                { "4", 4 },
+                { "5", 5 },
+                { "6", 6 },
+                { "7", 7 },
+                { "8", 8 },
+                { "9", 9 },
+            };
+
+            var lines = ParseInput.GetLinesFromFile(_filePath);
+            var counter = 0;
+            foreach (var line in lines)
+            {
+                var lowest = 10000;
+                var highest = -1;
+                var currentHigh = 0;
+                var current = 0;
+
+                foreach (var item in dict)
+                {
+                    var index = line.IndexOf($"{item.Key}");
+                    if (index != -1 && index < lowest)
+                    {
+                        lowest = index;
+                        current = item.Value;
+                    }
+                    var index2 = line.LastIndexOf($"{item.Key}");
+                    if (index2 != -1 && index2 > highest)
+                    {
+                        highest = index2;
+                        currentHigh = item.Value;
+                    }
+                }
+
+                var combined = string.Empty + current + currentHigh;
+                counter += int.Parse(combined);
+            }
+            return counter;
         }
     }
 }

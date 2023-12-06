c0ed88b89e5d323a06baf307113712910cc69df5
Cam Thompson <camstercammy@hotmail.com>
2023-12-06 09:38:43 -0500

    day 5

--- AdventOfCodeLibrary/AdventLibrary/Solution.cs
+++ AdventOfCodeLibrary/AdventLibrary/Solution.cs
@@ -9,13 +9,41 @@ namespace AdventLibrary
             Part1 = part1;
             Part2 = part2;
         }
+        public Solution(object part1, object part2, TimeSpan span1, TimeSpan span2)
+        {
+            Part1 = part1;
+            Part2 = part2;
+            Span1 = span1;
+            Span2 = span2;
+        }
 
         public object Part1;
 
         public object Part2;
 
+        public TimeSpan? Span1;
+
+        public TimeSpan? Span2;
+
         public void Output()
         {
+            Console.WriteLine("Part 1: " + Part1.ToString());
+            Console.WriteLine("Part 2: " + Part2.ToString());
+        }
+
+        public void Output2()
+        {
+            if (Span1 != null)
+            {
+                if (Span1.Value.Milliseconds > 1000)
+                {
+                    Console.WriteLine(string.Format("{0}:{1}", Math.Floor(Span1.Value.TotalMinutes), Span1.Value.ToString("ss\\.ff")));
+                }
+                else
+                {
+                    Console.WriteLine(Span1.Value.Milliseconds);
+                }
+            }
             Console.WriteLine("Part 1: " + Part1.ToString());
             Console.WriteLine("Part 2: " + Part2.ToString());
         }
--- Solutions/aoc2023/days/Day06.cs
+++ Solutions/aoc2023/days/Day06.cs
@@ -19,35 +19,58 @@ namespace aoc2023
         private object Part1()
         {
             var lines = ParseInput.GetLinesFromFile(_filePath);
-			var numbers = ParseInput.GetNumbersFromFile(_filePath);
-            var nodes = ParseInput.ParseFileAsGraph(_filePath);
-            var grid = ParseInput.ParseFileAsGrid(_filePath);
-            var total = 1000000;
-			var counter = 0;
-			
-			foreach (var line in lines)
-			{
-                var tokens = line.Split(delimiterChars);
-				var nums = AdventLibrary.StringParsing.GetNumbersFromString(line);
-                
-				foreach (var num in nums)
-				{
-				}
+            var times = AdventLibrary.StringParsing.GetNumbersFromString(lines[0]);
+            var distances = AdventLibrary.StringParsing.GetNumbersFromString(lines[1]);
+            var total = 1;
 
-                for (var i = 0; i < 0; i++)
+            for (var i = 0; i < times.Count; i++)
+            {
+                var counter = 0;
+                var time = times[i];
+                var distance = distances[i];
+                for (var j = 1; j < time; j++)
                 {
-                    for (var j = 0; j < 0; j++)
+                    var left = time - j;
+                    if (j*left > distance)
                     {
-                        
+                        counter++;   
                     }
                 }
-			}
-            return 0;
+                total *= counter;
+            }
+            return total;
         }
         
         private object Part2()
         {
-            return 0;
+            var lines = ParseInput.GetLinesFromFile(_filePath);
+            var times = AdventLibrary.StringParsing.GetNumbersFromString(lines[0]);
+            var totalTime = "";
+            foreach (var item in times)
+            {
+                totalTime += item;
+            }
+            var distances = AdventLibrary.StringParsing.GetNumbersFromString(lines[1]);
+            var totalDistance = "";
+            foreach (var item in distances)
+            {
+                totalDistance += item;
+            }
+            var total = 1;
+            var totDis = long.Parse(totalDistance);
+            var totTime = long.Parse(totalTime);
+
+            var counter = 0;
+            for (var j = 1; j < totTime; j++)
+            {
+                var left = totTime - j;
+                if (j * left > totDis)
+                {
+                    counter++;
+                }
+            }
+            total *= counter;
+            return total;
         }
     }
 }

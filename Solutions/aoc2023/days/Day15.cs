a81462bc5bd14cad3d87fbe3749912358ad64df4
Cam Thompson <camstercammy@hotmail.com>
2023-12-15 01:06:25 -0500

    day 15

--- Solutions/aoc2023/days/Day15.cs
+++ Solutions/aoc2023/days/Day15.cs
@@ -9,7 +9,8 @@ namespace aoc2023
     public class Day15: ISolver
     {
         private string _filePath;
-        private char[] delimiterChars = { ' ', ',', '.', ':', '-', '>', '<', '+', '=', '\t' };
+        private char[] delimiterChars = { ' ', ',', '.', ':', '>', '<', '+', '\t' };
+        private char[] delimiterChars2 = { ' ', ',', '.', ':', '-', '>', '<', '+', '=', '\t' };
         public Solution Solve(string filePath)
         {
             _filePath = filePath;
@@ -19,42 +20,101 @@ namespace aoc2023
         private object Part1()
         {
             var lines = ParseInput.GetLinesFromFile(_filePath);
-			var numbers = ParseInput.GetNumbersFromFile(_filePath);
-            var nodes = ParseInput.ParseFileAsGraph(_filePath);
-            var grid = ParseInput.ParseFileAsGrid(_filePath);
-            var total = 1000000;
-			var counter = 0;
-            
-            var ln1 = lines[0];
-            var ln2 = lines[1];
-            for (var i = 0; i < lines.Count; i++)
-            {
-                        
-            }
-			
-			foreach (var line in lines)
-			{
-                var tokens = line.Split(delimiterChars).ToList().OnlyRealStrings(delimiterChars);
-				var nums = StringParsing.GetNumbersFromString(line);
-                
-				foreach (var num in nums)
-				{
-				}
 
-                for (var i = 0; i < 0; i++)
+            var tokens = lines[0].Split(delimiterChars).ToList().OnlyRealStrings(delimiterChars);
+            var total = 0;
+            foreach (var item in tokens)
+            {
+                var current = 0;
+                for (var i = 0; i < item.Length; i++)
                 {
-                    for (var j = 0; j < 0; j++)
+                    if (item[i] != '\n')
                     {
-                        
+                        var intCode = (int)item[i];
+                        current += intCode;
+                        current *= 17;
+                        current = current % 256;
                     }
                 }
-			}
-            return 0;
+                total += current;
+            }
+            return total;
         }
         
         private object Part2()
         {
-            return 0;
+            var boxes = new List<List<string>>();
+            var boxes2 = new List<Dictionary<string, int>>();
+            var labelLookup = new Dictionary<string, int>();
+            for (var j = 0; j < 256; j++)
+            {
+                boxes.Add(new List<string>());
+                boxes2.Add(new Dictionary<string, int>());
+            }
+            var lines = ParseInput.GetLinesFromFile(_filePath);
+
+            var tokens = lines[0].Split(delimiterChars).ToList().OnlyRealStrings(delimiterChars);
+            var total = 0;
+            foreach (var item in tokens)
+            {
+                var toks = item.Split(delimiterChars2).ToList().OnlyRealStrings(delimiterChars2);
+                var label = toks[0];
+                var hash = 0;
+                for (var i = 0; i < toks[0].Length; i++)
+                {
+                    if (toks[0][i] != '\n')
+                    {
+                        var intCode = (int)toks[0][i];
+                        hash += intCode;
+                        hash *= 17;
+                        hash = hash % 256;
+                    }
+                }
+                
+                if (toks.Count == 2)
+                {
+                    var focal = int.Parse(toks[1]);
+                    if (boxes2[hash].ContainsKey(label))
+                    {
+                        boxes2[hash][label] = focal;
+                    }
+                    else
+                    {
+                        boxes[hash].Add(label);
+                        boxes2[hash].Add(label, focal);
+                    }
+
+                }
+                else
+                {
+                    if (boxes2[hash].ContainsKey(label))
+                    {
+                        var index = boxes[hash].IndexOf(label);
+
+                        if (index != -1)
+                        {
+                            boxes[hash].RemoveAt(index);
+                            labelLookup.Remove(label);
+                            boxes2[hash].Remove(label);
+                        }
+                    }
+                }
+            }
+            var bigTotal = 0;
+            for (var j = 0; j < 256; j++)
+            {
+                var county = 1;
+                foreach (var item in boxes[j])
+                {
+                    var item1 = 1 + j;
+                    var item2 = county;
+                    var item3 = boxes2[j][item];
+                    bigTotal += item1 * item2 * item3;
+
+                    county++;
+                }
+            }
+            return bigTotal;
         }
     }
 }
